90066a8c525810a500295c9ba389a07b2c292932
Gahasa Purba <gahasapurba.tech@gmail.com>
2022-09-06 16:35:18 +0700

    Menyusun direktori dan pembuatan controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomepageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Beranda Homepage
+Route::get('/', [HomepageController::class, 'index'])->name('homepage.index');
